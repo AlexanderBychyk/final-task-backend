9d9cddab7224bb6115a8d8a354415f43b6b6c3e9
Alexander Bychyk <sanya55bychyk@gmail.com>
2022-09-12 21:59:11 +0300

    feat: update string

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,10 +3,13 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = 'AmazingSanya';
+const Password = 'AS55cool';
+const clasterInfo = 'projectmanagementsystem.zcnl8tt';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
